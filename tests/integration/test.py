5f129caee7cf9ab311fb4d82e84ec06bf3ca75de
Yury Konov <gkonoff@gmail.com>
2025-03-20 04:28:05 +0300

    Remove all test files related to TestTable model.

--- tests/integration/test.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import Field, SQLModel
-
-
-class TestTable(SQLModel, table=True):
-    __tablename__ = "test_table"
-
-    id: int = Field(default=None, primary_key=True)
-    name: str
